dd018b0c31d4100d8183ebfab96de3a5277501b6
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-09-20 14:27:03 -0700

    Rework multiplayer shootdowns

--- Dogfighting addon.js
+++ Dogfighting addon.js
@@ -1,10 +1,12 @@
+//Add a Huey with guns (as an option)
 geofs.animation.values.gunsOn = null;
 function gunSound() {
 if (geofs.animation.values.groundContact == 0 && geofs.animation.values.brakes == 1) {
-   if (geofs.aircraft.instance.id == 7 || geofs.aircraft.instance.id == 18 || geofs.aircraft.instance.id == 15 || geofs.aircraft.instance.id == 2581 || geofs.aircraft.instance.id == 2808 || geofs.aircraft.instance.id == 3591 || geofs.aircraft.instance.id == 4172 || geofs.aircraft.instance.id == 3617) {
+   if (geofs.aircraft.instance.id == 7 || geofs.aircraft.instance.id == 18 || geofs.aircraft.instance.id == 15 || geofs.aircraft.instance.id == 2581 || geofs.aircraft.instance.id == 2808 || geofs.aircraft.instance.id == 3591 || geofs.aircraft.instance.id == 4172 || geofs.aircraft.instance.id == 3617 || geofs.aircraft.instance.id == 4251) {
 	geofs.animation.values.gunsOn = 1
    audio.impl.html5.playFile("https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/mirage-2000_sounds_m-2000-guns.wav")
 }
+//this is the special one...
    if (geofs.aircraft.instance.id == 2310) {
 geofs.animation.values.gunsOn = 1
 audio.impl.html5.playFile("https://142420819-645052386429616373.preview.editmysite.com/uploads/1/4/2/4/142420819/brrrt.mp3")
@@ -18,9 +20,10 @@ gunSoundInt = setInterval(function(){gunSound()},1000)
 //define functions
 function crashAircraft() {
    geofs.aircraft.instance.engines.forEach(function(e){
-e.thrust = 0
+e.maxRPM = 1
 	})
    new geofs.fx.ParticleEmitter({ anchor: { worldPosition: [0, 0, 0] }, duration: 1e3, rate: 0.01, life: 1e4, near: 10, startScale: 0.05, endScale: 1, startOpacity: 0.5, endOpacity: 1e-4, texture: "darkSmoke" });
+	audio.playShutdown();
 };
 function radians(n) {
   return n * (Math.PI / 180);
@@ -43,7 +46,13 @@ function getBearing(a, b, c, d) {
   } 
   return (degrees(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
 };
-
+function radToHdg(n) {
+  if (n < 0) {
+return (n * -1) + 180
+  } else {
+return n
+  }
+};
 //main function
 /*Value list of Object.values(multiplayer.visibleUsers)[0]
 e.callsign
@@ -51,28 +60,43 @@ e.aircraft (returns id)
 e.distance
 e.referencePoint.lla[0] and [1] for lla
 e.referencePoint.lla[2] and geofs.aircraft.instance.llaLocation[2] for altitude in meters
-line 17324 for other info?
 */
+// map.updatePlayerMarker
+//line 21000 for multiplayer update bullshit
+
+var airToAirOn = new Boolean(0)
+var shotDownUser = null
 var shootdownNotification = new Boolean(0);
-function checkAim() { //needs toggle option in options panel - just like advanced 2d clouds
+function checkAim() {
    Object.values(multiplayer.visibleUsers).forEach(function(e){
-	//geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])
-	if (e.referencePoint.lla[2] - geofs.aircraft.instance.llaLocation[2] <= 250 && geofs.aircraft.instance.llaLocation[2] - e.referencePoint.lla[2] <= 250 && e.distance <= 3000) { //same altitude, in distance
+//If the two users are at the same altitude and within gun range (I don't want to do triginometric interpolation)
+	if (e.referencePoint.lla[2] - geofs.aircraft.instance.llaLocation[2] <= 250 && geofs.aircraft.instance.llaLocation[2] - e.referencePoint.lla[2] <= 250 && e.distance <= 1000) {
+//If you're pointed at them and shooting your gun, shoot them down
 if ((geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])) <= 5 && (geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])) >= -5 && geofs.animation.values.gunsOn == 1) { //place smoke emitter on "enemy" player
 	 if (shootdownNotification == 0) {
 	    ui.notification.show("You shot down " + e.callsign)
 		 shootdownNotification = 1
+		 //This just makes sure that ui.notifications don't clog up on top of each other.
 		 setTimeout(() => {shootdownNotification = 0},1000)
 	 }
+	 shotDownUser = e.id
 };
+//This is some compelex gobbledygook - let me explain.
+//Essentially, it judges an aircraft's heading by checking the bearing between its position 1 second ago and its position now.
+//Then, it checks if that heading matches the bearing to that user.
+//It essentially checks if the user is doing what the above code statement is checking for.
+//Also, it double-checks that this isn't the user that you just shot down.
+var lastLLA = [e.referencePoint.lla[0],e.referencePoint.lla[1]]
 if (e.aircraft == 7 || e.aircraft == 18 || e.aircraft == 15 || e.aircraft == 2581 || e.aircraft == 2808 || e.aircraft == 3591 || e.aircraft == 4172 || e.aircraft == 3617) {
-	if ((geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])) <= -175 && (geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])) >= 175) {
-//shot-down players cannot shoot you down in turn
-//needs a toggle option or circumstance
+setTimeout(() => {
+	if (radToHdg(getBearing((lastLLA[0], lastLLA[1], e.referencePoint.lla[0], e.referencePoint.lla[1])) + radToHdg(geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])) <= 5 && radToHdg(getBearing(lastLLA[0], lastLLA[1], e.referencePoint.lla[0], e.referencePoint.lla[1])) - radToHdg(geofs.animation.values.heading - getBearing(e.referencePoint.lla[0], e.referencePoint.lla[1], geofs.aircraft.instance.llaLocation[0], geofs.aircraft.instance.llaLocation[1])) >= 5 && e.id != shotDownUser) {
 crashAircraft()
 audio.impl.html5.playFile("https://www.shockwave-sound.com/sound-effects/explosion-sounds/damage.wav")
+audio.impl.html5.playFile("https://www.shockwave-sound.com/sound-effects/explosion-sounds/damage.wav")
 ui.notification.show("You were shot down by " + e.callsign)
+console.log("shootdown")
 	}
+},1000)
 }
    }
 	})
