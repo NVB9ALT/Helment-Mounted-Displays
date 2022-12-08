022cf092746aa698e4410fdd334376aff844faea
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-12-08 07:35:45 -0800

    Update Dogfighting addon.js

--- Dogfighting addon.js
+++ Dogfighting addon.js
@@ -150,3 +150,27 @@ if (e.lastUpdate.st.gr == 1 && shootdownNotification == 0 && (e.aircraft == 7 ||
 };
 //Run checkAim at 20 FPS/TPS/whatever, this is fast enough for it to not miss things most of the time but also not lag your computer
 goInt = setInterval(function(){checkAim()},50);
+
+//clearInterval(cameraRotateInt)
+var needCamReset = new Boolean(0)
+function look() {
+   if (geofs.camera.currentModeName == "cockpit" && geofs.animation.values.accZ >= 40) {
+geofs.camera.setRotation(geofs.animation.values.roll * 100, (geofs.animation.values.pitch * 125)-10, 0)
+needCamReset = 1
+   } else if (geofs.camera.currentModeName == "cockpit" && needCamReset == 1) {
+geofs.camera.setRotation(geofs.aircraft.instance.definition.cameras.cockpit.orientation[0], geofs.aircraft.instance.definition.cameras.cockpit.orientation[1], geofs.aircraft.instance.definition.cameras.cockpit.orientation[2])
+needCamReset = 0
+	} else {
+needCamReset = 0
+	}
+}
+cameraRotateInt = setInterval(function(){look()},100)
+
+// C cycles between just follow and cockpit
+geofs.camera.cycle = function(){
+   if (geofs.camera.currentModeName == "cockpit") {
+geofs.camera.set(0)
+   } else {
+geofs.camera.set(1)
+	}
+}
